15cb74eeceac50af5935b430d69cc320bc63a1b7
Belo_cg86@itstepru.onmicrosoft.com <Belo_cg86@itstepru.onmicrosoft.com>
2022-07-25 18:42:55 +0500

    Добавьте файлы проекта.

---
+++ Massiv_hw/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Massiv_hw
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            int count = Uchastniki.vod_age("Введите кол-во участников: ");
+            List<Uchastniki> uchastniki = new List<Uchastniki>();
+            for (int i = 0; i < count; i++)
+            {
+                uchastniki.Add(new Uchastniki(i + 1));
+            }
+
+            void vyvod()
+            {
+                Console.WriteLine("Все участники: ");
+                foreach (var item in uchastniki)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+
+            Console.WriteLine("\nВсе участники: ");
+            foreach (var item in uchastniki)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("" +
+                "[1] Добавить участника\n" +
+                "[2] Удалитьт участника\n" +
+                "[3] Редактировать возраст участника\n" +
+                "[4] Редактировать имя учстника");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    uchastniki.Add(new Uchastniki(uchastniki.Count + 1));
+                    vyvod(); break;
+                case "2":
+                    Console.WriteLine("Введит ID участника: ");
+                    uchastniki.RemoveAt(Uchastniki.vod_age("") - 1);
+                    vyvod(); break;
+                case "3":
+                    Console.WriteLine("Введит ID участника: ");
+                    int index_age = (int.Parse(Console.ReadLine()) - 1);
+                    Console.WriteLine("Введите новый возраст: ");
+                    (uchastniki.ElementAt(index_age)).Age=int.Parse(Console.ReadLine());
+                    vyvod();break;
+                case "4":
+                    Console.WriteLine("Введит ID участника: ");
+                    int index_name=(int.Parse(Console.ReadLine())-1);
+                    Console.WriteLine("Введите новое имя: ");
+                    uchastniki.ElementAt(index_name).Name= String.Format(Console.ReadLine()); 
+                    vyvod(); break;
+                     default: break;                           
+
+            }
+            
+        }
+    }
+}
+
---
+++ Massiv_hw/Uchastniki.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massiv_hw
+{
+    internal class Uchastniki
+    {
+        public Uchastniki(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public Uchastniki(int id)
+        {
+            Id = id;
+            Name = vod("Введите Имя участника:  ");
+
+            Age = vod_age("Введите возраст участника:  ");
+        }
+
+        string vod(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+        public static int vod_age(string text)
+        {
+            try
+            {
+                Console.Write(text);
+                return int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("что то пошло не так");
+                return vod_age(text);
+
+            }
+        }
+
+        //свойства
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID Участника: {Id} \nИмя: {Name}\nВозраст: {Age}\n";
+        }
+
+
+    }
+}
